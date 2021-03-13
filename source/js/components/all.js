8ab77d3575fba7f0a1211cc3f39ed717d5fd6dbc
Ralph Samaniego <rapsamaniego27@gmail.com>
2021-03-13 09:45:27 +0800

    added smooth scrolling

--- source/js/components/all.js
+++ source/js/components/all.js
@@ -1,6 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
   const infoSkills = document.querySelector('#infoSkills');
   const welcomeContent = document.querySelector('.welcome__content');
+  const bioMapper = document.querySelector('#bioMapper');
 
   
   if (isInPage(infoSkills)) {
@@ -11,11 +12,41 @@ window.addEventListener('DOMContentLoaded', () => {
 
   }
 
+  /* Homepage */
   if (isInPage(welcomeContent)) {
 
     const animate = new Animate();
     animate.welcome();
     animate.noteSet();
+
+  }
+
+  /* Biography page */
+  if (isInPage(bioMapper)) {
+
+    const bioSections = document.querySelectorAll('.bio-section');
+    const bioMapperBtns = document.querySelectorAll('.bio-mapper__item');
+    const scroll = new Scroll();
+  
+    bioMapperBtns.forEach(btn => {
+      btn.addEventListener('click', (e) => {
+        const id = btn.id;
+        e.preventDefault();
+        scroll.smoothScroll(`.${id}`, 1000);
+        console.log(id);
+        
+        
+        /* Remove former active class and apply to new one */
+        const formerActiveBtn = document.querySelector('.bio-mapper__item--active');
+        formerActiveBtn.classList.remove('bio-mapper__item--active'); 
+        
+        btn.classList.add('bio-mapper__item--active');
+        
+
+      });
+    });
+
+    
 
   }
 
